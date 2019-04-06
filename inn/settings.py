e94ed93e2a6a847629ab7a55ba810c31433e8e80
Mugaya Joel <josean.patrick11@gmail.com>
2019-04-06 21:54:20 +0300

    Update settings.py

--- inn/settings.py
+++ inn/settings.py
@@ -13,6 +13,7 @@ black . --exclude /venv
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,6 +41,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    'rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
 ]
 
 MIDDLEWARE = [
@@ -108,6 +114,19 @@ REST_FRAMEWORK = {
 }
 
 # JWT_AUTH = {"JWT_AUTH_HEADER_PREFIX": "Bearer"}
+# Configure the JWTs to expire after 1 hour, and allow users to refresh near-expiration tokens
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=1),
+    'JWT_ALLOW_REFRESH': True,
+}
+
+
+# Enables django-rest-auth to use JWT tokens instead of regular tokens.
+REST_USE_JWT = True
+
+
+#required for the django.contrib.sites dependency.
+SITE_ID = 1
 
 
 # Password validation
