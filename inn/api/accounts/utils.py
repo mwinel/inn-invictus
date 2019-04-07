7112f480a49159759589474268b503785b49c0be
Mugaya Joel <josean.patrick11@gmail.com>
2019-04-07 12:11:25 +0300

    Update utils.py

--- inn/api/accounts/utils.py
+++
@@ -1,9 +0,0 @@
-from .serializer import UserSerializer
-
-
-def jwt_response_payload_handler(token, user=None, request=None):
-    """ JWT_RESPONSE_PAYLOAD_HANDLER """
-    return {
-        "token": token,
-        "user": UserSerializer(user, context={"request": request}).data,
-    }
