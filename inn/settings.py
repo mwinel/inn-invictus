02d56422a6c2682d994cd4ecbf70b4df58153dec
Mugaya Joel <josean.patrick11@gmail.com>
2019-04-07 21:32:13 +0300

    Update settings.py

--- inn/settings.py
+++ inn/settings.py
@@ -107,7 +107,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    
+
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
