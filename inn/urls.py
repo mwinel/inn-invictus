0e52511fc56908cd3c589c7bcacc41bd86d1ad80
Mugaya Joel <josean.patrick11@gmail.com>
2019-04-07 20:38:36 +0300

    Update urls.py

--- inn/urls.py
+++ inn/urls.py
@@ -1,29 +1,34 @@
-"""inn URL Configuration
 
-^accounts/password/reset/$ [name='rest_password_reset']
-^accounts/password/reset/confirm/$ [name='rest_password_reset_confirm']
-^accounts/login/$ [name='rest_login']
-^accounts/logout/$ [name='rest_logout']
-^accounts/user/$ [name='rest_user_details']
-^accounts/password/change/$ [name='rest_password_change']
-^accounts/signup/
-^accounts/users/
-^refresh-token/
-"""
-from django.conf.urls import url, include
-from inn.api.accounts import views
+from django.conf.urls import url
+from . import views
+from rest_auth import views as auth_views
+from rest_auth.registration.views import RegisterView, VerifyEmailView
 from rest_framework_jwt.views import refresh_jwt_token
-from allauth.account.views import confirm_email
-from django.views.generic.base import TemplateView
+from allauth.account.views import ConfirmEmailView
 
 urlpatterns = [
     url(r'^$', views.Home, name="home"),
-    url(r'^accounts/', include('rest_auth.urls')),
-    url(r'^accounts/signup/', include('rest_auth.registration.urls')),
+
+    url(r'^accounts/password/reset/$', auth_views.PasswordResetView.as_view(),
+        name='password_reset'),
+    url(r'^accounts/password/reset/confirm/$',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='confirm_account_logout'),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(),
+        name='account_login'),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(),
+        name='account_logout'),
+    url(r'^accounts/user/$', auth_views.UserDetailsView.as_view(),
+        name='account_user'),
+    url(r'^accounts/password/change/$', auth_views.PasswordChangeView.as_view(),
+        name='password_change'),
+
+    url(r'^accounts/signup/', RegisterView.as_view(), name='account_signup'),
     url(r'^refresh-token/$', refresh_jwt_token),
     url(r'^accounts/users/$', views.UserList.as_view(), name="users"),
-    # url(r'^accounts/signup/account-confirm-email/(?P<key>.+)/$',
-    url(r"^accounts/signup/account-confirm-email/(?P<key>[\s\d\w().+-_',:&]+)/$",
-        TemplateView.as_view(template_name='confirm_mail.html'),
-        confirm_email, name='account_confirm_email'),
+    url(r'^accounts/verify-email/$', VerifyEmailView.as_view(),
+        name='rest_verify_email'),
+    url(r'^accounts/account-confirm-email/(?P<key>[-:\w]+)/$',
+        ConfirmEmailView.as_view(),
+        name='account_confirm_email'),
 ]
