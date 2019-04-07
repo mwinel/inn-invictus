6966ac1e79fc62822c1dc27f6899fed7eb1cc5d7
Mugaya Joel <josean.patrick11@gmail.com>
2019-04-07 08:48:21 +0300

    Update views.py

--- inn/api/accounts/views.py
+++ inn/api/accounts/views.py
@@ -1,83 +1,18 @@
-from rest_framework.views import APIView
-from rest_framework import generics
+# from rest_framework.views import APIView
+# from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
-from django.contrib.auth import login
-from .serializer import UserSerializer, LoginSerializer
-from rest_framework import status
-from django.contrib.auth.password_validation import ValidationError
+from .serializer import UserSerializer
 
 
-class HelloView(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request):
-        content = {'message': 'Hello, World!'}
-        return Response(content)
+@api_view(['GET',])
+def Home(request):
+    content = {'message': 'My Hello, World App!'}
+    return Response(content)
 
 
-class UserList(generics.ListAPIView):
+class UserList(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class Login(APIView):
-    # queryset = User.objects.all()
-    serializer_class = LoginSerializer
-
-    def post(self, request):
-
-        serializer = LoginSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = User.objects.get(username=serializer.data['username'])
-        login(request, user)
-
-        return Response(
-            data={"Username": serializer.data['username'],
-                  "Email": serializer.data['email'],
-                  "token": serializer.data['token']},
-            status=status.HTTP_200_OK
-
-        )
-
-
-class SignUp(APIView):
-    serializer_class = UserSerializer
-    # queryset = User.objects.all()
-
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            try:
-                user = serializer.save()
-            except ValidationError as errors:
-                return Response(
-                        data={"status": 400,
-                              "errors": [errors]
-                              },
-                        status=status.HTTP_400_BAD_REQUEST,
-                )
-
-            return Response(
-                data={
-                    "status": status.HTTP_201_CREATED,
-                    "data": [
-                        {
-                            "username": user.username,
-                            "email": user.email,
-                            "is_admin": user.is_superuser,
-                        }
-                    ],
-                },
-                status=status.HTTP_201_CREATED,
-            )
-        return Response(
-            data={"status": 400, "errors": serializer.errors},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
