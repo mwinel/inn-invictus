ad4ba6345d363516c5d0956eb3fde885654736f0
Mugaya Joel <josean.patrick11@gmail.com>
2019-04-07 20:39:22 +0300

    deleteing accounts app

--- inn/api/accounts/serializer.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'is_active', 'is_superuser',)
-        read_only_fields = ('is_active', 'is_superuser',)
