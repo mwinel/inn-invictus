926c4f9f7b7e7edf99622f1d57bda0ca554b9b25
Mugaya Joel <josean.patrick11@gmail.com>
2019-04-07 12:11:31 +0300

    Update views.py

--- inn/api/accounts/views.py
+++ inn/api/accounts/views.py
@@ -1,5 +1,3 @@
-# from rest_framework.views import APIView
-# from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -7,7 +5,7 @@ from django.contrib.auth.models import User
 from .serializer import UserSerializer
 
 
-@api_view(['GET',])
+@api_view(['GET', ])
 def Home(request):
     content = {'message': 'My Hello, World App!'}
     return Response(content)
